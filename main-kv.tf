886c85523c89fdd509e966d2ec368044c1e4d1cc
Benoit Blais <59298977+benyboy84@users.noreply.github.com>
2023-07-28 09:55:51 -0400

    Update main-kv.tf

---
+++ main-kv.tf
@@ -0,0 +1,42 @@
+# ------------------------------------------------------------------------------
+# Creating Key Vault
+# ------------------------------------------------------------------------------
+
+resource "azurerm_key_vault" "this" {
+  name                            = lower(var.name)
+  location                        = var.location
+  resource_group_name             = var.resource_group_name
+  tenant_id                       = data.azurerm_client_config.this.tenant_id
+  sku_name                        = var.sku_name
+  enabled_for_deployment          = var.enabled_for_deployment
+  enabled_for_disk_encryption     = var.enabled_for_disk_encryption
+  enabled_for_template_deployment = var.enabled_for_template_deployment
+  soft_delete_retention_days      = var.soft_delete_retention_days
+  purge_protection_enabled        = var.purge_protection_enabled
+  enable_rbac_authorization       = var.enable_rbac_authorization
+  public_network_access_enabled   = var.public_network_access_enabled
+  tags = merge(var.tags, {
+    creation_date        = "${time_static.this.year}-${time_static.this.month}-${time_static.this.day}"
+    managed_by_terraform = "true"
+  })
+
+  dynamic "network_acls" {
+    for_each = var.network_acls != null ? [true] : []
+    content {
+      bypass                     = var.network_acls.bypass
+      default_action             = var.network_acls.default_action
+      ip_rules                   = var.network_acls.ip_rules
+      virtual_network_subnet_ids = var.network_acls.virtual_network_subnet_ids
+    }
+  }
+
+  dynamic "contact" {
+    for_each = var.certificate_contacts
+    content {
+      email = contact.value.email
+      name  = contact.value.name
+      phone = contact.value.phone
+    }
+  }
+
+}
