a9d8ac6a1d31296ef4fbcb1d5db280658cf15c1b
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-08-21 15:57:23 -0400

    initial commit

--- examples/main.tf
+++ examples/main.tf
@@ -6,12 +6,12 @@ resource "azurerm_resource_group" "example" {
 data "azurerm_client_config" "this" {}
 
 module "keyvault" {
-  source  = "https://github.com/benyboy84/terraform-azurerm-key_vault?ref=v1.0.0"
+  source = "https://github.com/benyboy84/terraform-azurerm-key_vault?ref=v1.0.0"
 
-  name                            = "example"
-  location                        = azurerm_resource_group.example.location
-  resource_group_name             = azurerm_resource_group.example.name
-  enabled_for_deployment          = true
-  enabled_for_disk_encryption     = true
-  admin_objects_ids               = [data.azurerm_client_config.this.object_id]
+  name                        = "example"
+  location                    = azurerm_resource_group.example.location
+  resource_group_name         = azurerm_resource_group.example.name
+  enabled_for_deployment      = true
+  enabled_for_disk_encryption = true
+  admin_objects_ids           = [data.azurerm_client_config.this.object_id]
 }
